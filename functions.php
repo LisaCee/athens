a4b4f9cb472d533c27c35f167c945add6fd15d49
LisaCee <lisaceedesign@gmail.com>
2021-01-30 17:05:05 -0800

    enqueue stylesheets, fix typo

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
-<!-- enqueue style sheets here -->
 <?php 
 
 function load_css() {
-	wp_register_style( 'boostrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all' );
+	wp_register_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all' );
 	wp_enqueue_style( 'bootstrap' );
 }
 
@@ -13,7 +12,7 @@ function load_js() {
 	wp_enqueue_script( 'jquery' );
 
 	wp_register_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, true );
-	wp_enqueue_script( 'bootsrap' );
+	wp_enqueue_script( 'bootstrap' );
 }
 
 add_action( 'wp_enqueue_scripts', 'load_js' );
